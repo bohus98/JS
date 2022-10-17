6592b69f0b29596ceba57d12d879bd66eb34df39
bohus98 <58601109+bohus98@users.noreply.github.com>
2022-10-17 19:45:54 +0200

    Commit #1
    Day one commit

---
+++ Day1/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof 'string')
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ Day1/variable.js
@@ -0,0 +1,17 @@
+// let string = "aaa"
+// let boolean = true
+// let undefined
+// let empty = null
+
+// let firstName = 'Matus'
+// let lastName = 'Bohucky'
+// let maritalStatus = 'single'
+// let country = 'Czech'
+// let age = 24
+
+let firstName = 'Matus', lastName = 'Bohucky', maritalStatus = 'single',country = 'Czech', age = 24
+
+const myAge = 24
+const yourAge = 30
+console.log("I am", myAge, "years old.")
+console.log("You are", myAge, "years old.")
